1425e1806bd16c54cb5d0981ef94096f3862e628
kenkoooo <kenkou.n@gmail.com>
2019-01-08 23:17:40 +0900

    Added logzero

--- predict-xgb.py
+++ predict-xgb.py
@@ -5,8 +5,8 @@ import pandas as pd
 import re
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
-from tqdm import tqdm
 from sklearn.model_selection import KFold
+from logzero import logger
 
 RATED = "rated"
 AC_COUNT = "ac_count"
@@ -14,12 +14,12 @@ P_ID = "problem_id"
 U_ID = "user_id"
 IS_AC = "is_ac"
 SCORE = "score"
-R_POINT = "point"
+POINT = "point"
 
 FIRST_AGC_START = 1468670400
 HEAVY_USER_AC_COUNT_THRESHOLD = 200
 
-NORMAL_USER_PATTERN = "^(?!(luogu_bot|vjudge)\d+)"
+NORMAL_USER_PATTERN = r"^(?!(luogu_bot|vjudge)\d+)"
 
 
 def add_rated_column(contests):
@@ -35,17 +35,17 @@ def add_is_ac_column(submissions):
 
 
 def merge_submissions_contests(submissions, contests):
-    merged = pd.merge(submissions, contests,
-                      "left", left_on="contest_id", right_index=True)
-    merged = merged[[P_ID, U_ID, R_POINT, IS_AC, RATED]]
+    merged = pd.merge(submissions, contests, "left",
+                      left_on="contest_id", right_index=True, copy=False)
+    merged = merged[[P_ID, U_ID, POINT, IS_AC, RATED]]
     return merged
 
 
 def add_rated_point_column(table):
     rated_point = table[(table[RATED]) & (table[IS_AC])][[
-        R_POINT, P_ID]].groupby(P_ID).max()
+        POINT, P_ID]].groupby(P_ID).max()
     table = pd.merge(table, rated_point, "left",
-                     left_on=P_ID, right_index=True)
+                     left_on=P_ID, right_index=True, copy=False)
     return table, rated_point
 
 
@@ -55,15 +55,24 @@ def add_ac_count_column(table):
     ac_count = pd.DataFrame(ac_count).rename(
         columns={P_ID: AC_COUNT})
     table = pd.merge(table, ac_count, "left",
-                     left_on=U_ID, right_index=True)
+                     left_on=U_ID, right_index=True, copy=False)
     return table
 
 
+def create_one_hot_table(t):
+    p = pd.get_dummies(t[[P_ID, U_ID]], columns=[U_ID], dtype='int8')
+    p = p.groupby(P_ID).max()
+    p.reset_index(inplace=True)
+    return p
+
+
 def create_df():
+    logger.info("loading data...")
     contests = pd.read_json("data/contests.json").set_index("id")
     submissions = pd.read_csv("data/atcoder_submissions.csv")
     problem_ids = pd.read_json("data/problems.json")["id"].values
 
+    logger.info("merging tables...")
     contests = add_rated_column(contests)
     submissions = add_is_ac_column(submissions)
     table = merge_submissions_contests(submissions, contests)
@@ -72,22 +81,18 @@ def create_df():
     table = table[(table[AC_COUNT] >= HEAVY_USER_AC_COUNT_THRESHOLD)
                   & (table[U_ID].str.contains(NORMAL_USER_PATTERN))]
 
+    logger.info("calculating scores...")
     t = table[[P_ID, U_ID, IS_AC]].drop_duplicates()
     t[SCORE] = 0
     t[SCORE].where(t[IS_AC], -1, inplace=True)
     t[SCORE].mask(t[IS_AC], 1, inplace=True)
     t[[P_ID, U_ID, SCORE]].groupby([P_ID, U_ID]).max().reset_index()
-    p = pd.get_dummies(t[t[SCORE] > 0][[P_ID, U_ID]],
-                       columns=[U_ID], dtype='int8')
-    n = pd.get_dummies(t[t[SCORE] < 0][[P_ID, U_ID]],
-                       columns=[U_ID], dtype='int8')
-    p = p.groupby(P_ID).max()
-    n = n.groupby(P_ID).max()
-    p.reset_index(inplace=True)
-    n.reset_index(inplace=True)
-    n.replace(1, -1, inplace=True)
-    df = pd.concat([p, n]).groupby(P_ID).max()
 
+    logger.info("creating one hot table...")
+    p = create_one_hot_table(t[t[SCORE] > 0])
+    n = create_one_hot_table(t[t[SCORE] < 0]).replace(1, -1)
+
+    df = pd.concat([p, n]).groupby(P_ID).max()
     df = pd.merge(df, rated_point, "left", left_index=True, right_index=True)
 
     return df
@@ -97,20 +102,21 @@ def create_df():
 df = create_df()
 
 # %%
-train_data = df[df[R_POINT].notnull()]
-x = train_data.drop(R_POINT, axis=1).values
-y = train_data[R_POINT]
+logger.info("cross-validationg...")
+train_data = df[df[POINT].notnull()]
+x = train_data.drop(POINT, axis=1).values
+y = train_data[POINT]
 kf = KFold(n_splits=5, random_state=71, shuffle=True)
 for train_index, test_index in kf.split(train_data):
     x_train, y_train = x[train_index], y[train_index]
     x_test, y_test = x[test_index], y[test_index]
     model = xgb.XGBRegressor(seed=71)
     model.fit(x_train, y_train)
-    print(model.score(x_test, y_test))
+    logger.info(model.score(x_test, y_test))
 
 # %%
 importance = pd.DataFrame(model.feature_importances_,
-                          index=df.drop(R_POINT, axis=1).columns,
+                          index=df.drop(POINT, axis=1).columns,
                           columns=["importance"])
 importance[importance["importance"] > 0.0].sort_values(
     by="importance", ascending=False)
